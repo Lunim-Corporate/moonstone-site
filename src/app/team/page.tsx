a210abf87e4e11dbece13988ad47f0b3a4002ac5
Andy Agyei-Dwumah <andydwumah@gmail.com>
2025-11-26 00:06:11 +0000

    feat: enhance Team slice component with dynamic rendering and Prismic integration

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -1,5 +1,20 @@
-export default function Page() {
+// Prismicio
+import { createClient } from "@/src/prismicio"
+// Slices
+import { components } from "@/src/slices"
+// Prismic
+import { SliceZone } from "@prismicio/react"
+// Next
+import { notFound } from "next/navigation"
+
+export default async function Page() {
+  const client = createClient()
+  const doc = await client.getByUID("page", "team")
+  if (!doc) notFound()
+
   return (
-    <div>page</div>
+    <main>
+      <SliceZone slices={doc.data.slices} components={components} />
+    </main>
   )
 }
--- src/slices/Team/index.tsx
+++ src/slices/Team/index.tsx
@@ -1,6 +1,7 @@
-import { FC } from "react";
+// Prismic
 import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { PrismicNextImage } from "@prismicio/next";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 
 /**
  * Props for `Team`.
@@ -10,21 +11,36 @@ export type TeamProps = SliceComponentProps<Content.TeamSlice>;
 /**
  * Component for "Team" Slices.
  */
-const Team: FC<TeamProps> = ({ slice }) => {
+export default function Team({ slice }: TeamProps) {
   return (
-    <section
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
-    >
-      Placeholder component for team (variation: {slice.variation}) slices.
-      <br />
-      <strong>You can edit this slice directly in your code editor.</strong>
-      {/**
-       * 💡 Use the Prismic MCP server with your code editor
-       * 📚 Docs: https://prismic.io/docs/ai#code-with-prismics-mcp-server
-       */}
-    </section>
+    <div className="max-w-4xl mx-auto">
+      <div className="mb-14 max-w-lg text-center mx-auto">
+        <PrismicRichText
+          field={slice.primary.heading}
+          components={{
+            heading1: ({ children }) => <h1 className="text-center mb-5">{children}</h1>
+          }}
+        />
+        <PrismicRichText field={slice.primary.body_text} />
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-y-20 sm:gap-4">
+        {slice.primary.team_member.map((teamMember, idx) => (
+            <div key={idx} className="rounded overflow-hidden bg-[#2d2b2b]">
+              <div className="relative mb-4">
+                <PrismicNextImage field={teamMember.headshot} />
+                <div className="px-5 absolute bottom-0 py-6">
+                  <PrismicRichText field={teamMember.name} />
+                </div>
+              </div>
+              <div className="px-5">
+                <div className="mb-8">
+                <PrismicRichText field={teamMember.role} />
+                </div>
+              <PrismicRichText field={teamMember.bio} components={{paragraph: ({children}) => <p className="mb-2 last:mb-0 pb-6">{children}</p>}} />
+              </div>
+            </div>
+        ))}
+      </div>
+    </div>
   );
 };
-
-export default Team;
