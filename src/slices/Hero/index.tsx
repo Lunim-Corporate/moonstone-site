47bfcc6c4258be5aa14dff00efca75f44533c206
Andy Agyei-Dwumah <andydwumah@gmail.com>
2025-11-27 10:33:16 +0000

    fix: adjust layout and styling in Hero component for improved responsiveness

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -16,16 +16,19 @@ export default function Hero({ slice }: HeroProps) {
       style={{
         backgroundImage: `url(${slice.primary.background_image.url})`,
       }}
-      className="bg-cover bg-center min-h-screen grid items-center"
+      className="bg-cover bg-center min-h-screen relative"
     >
+      <div className="min-h-screen grid items-center absolute inset-0 bg-[rgba(0,0,0,0.5)]">
         <div className="max-w-4xl text-center mx-auto">
-          <PrismicRichText 
+          <PrismicRichText
             field={slice.primary.heading}
-          components={{
-            heading1: ({ children }) => <h1 className="mb-5">{children}</h1>
-            }} />
+            components={{
+              heading1: ({ children }) => <h1 className="mb-5">{children}</h1>,
+            }}
+          />
           <PrismicRichText field={slice.primary.body} />
         </div>
+      </div>
     </div>
   );
 };
