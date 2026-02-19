a65a6920f947a6cbbd65f52e064c9979b4d26b54
Asish <66243448+assh@users.noreply.github.com>
2026-02-19 12:45:37 +0000

    Add bugsnag

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,20 @@
 "use client";
 
+import { useEffect } from "react";
 import { SessionProvider } from "next-auth/react";
+import Bugsnag from "@bugsnag/js";
+
+let bugsnagStarted = false;
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    if (!bugsnagStarted) {
+      Bugsnag.start({
+        apiKey: process.env.NEXT_PUBLIC_BUGSNAG_API_KEY ?? '125d4e1a653190795d94c6d854a434d4',
+      });
+      bugsnagStarted = true;
+    }
+  }, []);
+
   return <SessionProvider>{children}</SessionProvider>;
 }
